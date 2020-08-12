d778e965fc2f32b47fb5149aa95686a311b7ae39
HayeGregory <haye.greg@outlook.com>
2020-08-12 16:54:29 +0200

    ex 08 - function table color

---
+++ base/ex08.js
@@ -0,0 +1,16 @@
+function printTableColors(idDiv, b) {
+    var divColorPick = document.getElementById(idDiv, b);
+    var tableColor = divColorPick.appendChild(document.createElement("table"));
+    var gap = 16;
+
+    //divColorPick.appendChild(tableColor);
+    for(var i = 0; i < (256/gap); i++) {
+        var tr = tableColor.appendChild(document.createElement("tr"));
+        
+        for(var j = 0; j < (256/gap); j++) {
+            var td = tr.appendChild(document.createElement("td"));
+            td.setAttribute("style", `width:16px; height:16px; background-color:rgb(${i*gap}, ${j*gap}, ${b});` );
+        }    
+    }
+}
+
